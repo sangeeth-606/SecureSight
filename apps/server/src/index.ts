c05841e6705042b91daa0e9de3d6f75c6bf2f4fe
sangeeth-606 <sangeeth999123@gmail.com>
2025-07-23 05:01:07 +0530

    server: refactor server logic and update dependencies

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,20 +1,24 @@
 
-import express from "express";
-
-import "dotenv/config";
-
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import 'dotenv/config';
+import incidentRoutes from './routes/incidents';
 
 const app = express();
 
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api', incidentRoutes);
+
 app.listen(3002, () => {
-  console.log("Server running at http://localhost:3002");
+  console.log('Server running at http://localhost:3002');
 });
